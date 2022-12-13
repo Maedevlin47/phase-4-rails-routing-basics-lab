b00ca8b7dff2614f3c3d5aa5451912b36a1294c3
Mae Schreck <maedevlin47@gmail.comm>
2022-12-12 22:25:02 -0500

    commit tree f74f783a2a6c2cabe635ba35f1da9da1729bdb7d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/students', to: 'students#index'
+  #http://localhost:3000/students
+
+  get '/students/grades', to: 'students#grades'
+  #http://localhost:3000/students#students#grades
+
+  get '/students/highest-grade', to: 'students#highest_grade'
+  #http://localhost:3000/students#students#hightestgrades
 end
